e8b7ac33e098f522dcbddff8276ed6606a751a9f
龙腾猫跃 <1043137532@qq.com>
2026-04-21 19:39:33 +0800

    ref: 升级到 C# 语言版本 12

--- Extensions/EnumerableExtensions.cs
+++ Extensions/EnumerableExtensions.cs
@@ -4,182 +4,176 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MeloongCore.Extensions {
-    public static class EnumerableExtensions {
+namespace MeloongCore.Extensions;
+public static class EnumerableExtensions {
 
-        #region Distinct
+    #region Distinct
 
-        /// <summary>
-        /// 通过比较器进行去重。
-        /// 该方法的效率较低，建议仅在小型列表上使用，或换用 DistinctBy。
-        /// </summary>
-        public static IEnumerable<T> Distinct<T>(this IList<T> list, Func<T, T, bool> comparer) {
-            for (int i = 0; i < list.Count; i++) {
-                if (!list.Skip(i + 1).Any(item => comparer(list[i], item))) yield return list[i];
-            }
+    /// <summary>
+    /// 通过比较器进行去重。
+    /// 该方法的效率较低，建议仅在小型列表上使用，或换用 DistinctBy。
+    /// </summary>
+    public static IEnumerable<T> Distinct<T>(this IList<T> list, Func<T, T, bool> comparer) {
+        for (int i = 0; i < list.Count; i++) {
+            if (!list.Skip(i + 1).Any(item => comparer(list[i], item))) yield return list[i];
         }
-        /// <summary>
-        /// 按对象的指定值去重。
-        /// </summary>
-        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector) {
-            var seen = new HashSet<TKey>();
-            foreach (var element in source) {
-                if (seen.Add(selector(element))) yield return element;
-            }
-        }
-
-        #endregion
-
-        #region MinBy / MaxBy
-
-        /// <summary>
-        /// 选择所有最大值对应的对象。
-        /// 若没有元素则返回空列表。
-        /// </summary>
-        public static List<T> MaxByAll<T, C>(this IEnumerable<T> source, Func<T, C> selector) where C : IComparable<C> {
-            var results = new List<T>();
-            using (var enumerator = source.GetEnumerator()) {
-                if (!enumerator.MoveNext()) return results;
-                T maxItem = enumerator.Current;
-                C maxValue = selector(maxItem);
-                results.Add(maxItem);
-                while (enumerator.MoveNext()) {
-                    T currentItem = enumerator.Current;
-                    C currentValue = selector(currentItem);
-                    int comparisonResult = currentValue.CompareTo(maxValue);
-                    if (comparisonResult > 0) {
-                        maxValue = currentValue;
-                        results.Clear();
-                        results.Add(currentItem);
-                    } else if (comparisonResult == 0) {
-                        results.Add(currentItem);
-                    }
-                }
-            }
-            return results;
-        }
-        /// <summary>
-        /// 选择所有最小值对应的对象。
-        /// 若没有元素则返回空列表。
-        /// </summary>
-        public static List<T> MinByAll<T, C>(this IEnumerable<T> source, Func<T, C> selector) where C : IComparable<C> {
-            var results = new List<T>();
-            using (var enumerator = source.GetEnumerator()) {
-                if (!enumerator.MoveNext()) return results;
-                T minItem = enumerator.Current;
-                C minValue = selector(minItem);
-                results.Add(minItem);
-                while (enumerator.MoveNext()) {
-                    T currentItem = enumerator.Current;
-                    C currentValue = selector(currentItem);
-                    int comparisonResult = currentValue.CompareTo(minValue);
-                    if (comparisonResult < 0) {
-                        minValue = currentValue;
-                        results.Clear();
-                        results.Add(currentItem);
-                    } else if (comparisonResult == 0) {
-                        results.Add(currentItem);
-                    }
-                }
-            }
-            return results;
-        }
-
-        /// <summary>
-        /// 选择最大值对应的对象。
-        /// 若没有元素则返回 Nothing。
-        /// </summary>
-        public static T MaxBy<T, C>(this IEnumerable<T> source, Func<T, C> selector) where C : IComparable<C> {
-            using (var enumerator = source.GetEnumerator()) {
-                if (!enumerator.MoveNext()) return default;
-                T maxItem = enumerator.Current;
-                C maxValue = selector(maxItem);
-                while (enumerator.MoveNext()) {
-                    C value = selector(enumerator.Current);
-                    if (value.CompareTo(maxValue) <= 0) continue;
-                    maxItem = enumerator.Current;
-                    maxValue = value;
-                }
-                return maxItem;
-            }
-        }
-        /// <summary>
-        /// 选择最小值对应的对象。
-        /// 若没有元素则返回 Nothing。
-        /// </summary>
-        public static T MinBy<T, C>(this IEnumerable<T> List, Func<T, C> Selector) where C : IComparable<C> {
-            using (var enumerator = List.GetEnumerator()) {
-                if (!enumerator.MoveNext()) { return default; }
-                T minItem = enumerator.Current;
-                C minValue = Selector(minItem);
-                while (enumerator.MoveNext()) {
-                    C value = Selector(enumerator.Current);
-                    if (value.CompareTo(minValue) >= 0) { continue; }
-                    minItem = enumerator.Current;
-                    minValue = value;
-                }
-                return minItem;
-            }
-        }
-
-        #endregion
-
-        #region Join
-
-        /// <summary>
-        /// 用指定的分割符将集合连接为字符串。
-        /// </summary>
-        public static string Join(this IEnumerable list, char split) {
-            StringBuilder builder = new StringBuilder();
-            bool isFirst = true;
-            foreach (var element in list) {
-                if (isFirst)
-                    isFirst = false;
-                else
-                    builder.Append(split);
-                if (element != null) builder.Append(element.ToString());
-            }
-            return builder.ToString();
-        }
-        /// <summary>
-        /// 用指定的分割符将集合连接为字符串。
-        /// </summary>
-        public static string Join(this IEnumerable list, string split) {
-            if (split.IsSingle()) return list.Join(split[0]);
-            StringBuilder builder = new StringBuilder();
-            bool isFirst = true;
-            foreach (var element in list) {
-                if (isFirst)
-                    isFirst = false;
-                else
-                    builder.Append(split);
-                if (element != null) builder.Append(element.ToString());
-            }
-            return builder.ToString();
-        }
-
-        #endregion
-
-        /// <summary>
-        /// 判断集合是否有且仅有一个元素。
-        /// </summary>
-        public static bool IsSingle<T>(this IEnumerable<T> source) {
-            if (source == null) return false;
-            if (source is IList<T> list) return list.Count == 1;
-            using (var enumerator = source.GetEnumerator()) {
-                if (!enumerator.MoveNext()) return false;
-                if (enumerator.MoveNext()) return false;
-                return true;
-            }
-        }
-
-        /// <summary>
-        /// 对集合的每个元素执行指定操作。
-        /// </summary>
-        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action) {
-            foreach (T item in source) action(item);
-            return source;
-        }
-
     }
+    /// <summary>
+    /// 按对象的指定值去重。
+    /// </summary>
+    public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector) {
+        var seen = new HashSet<TKey>();
+        foreach (var element in source) {
+            if (seen.Add(selector(element))) yield return element;
+        }
+    }
+
+    #endregion
+
+    #region MinBy / MaxBy
+
+    /// <summary>
+    /// 选择所有最大值对应的对象。
+    /// 若没有元素则返回空列表。
+    /// </summary>
+    public static List<T> MaxByAll<T, C>(this IEnumerable<T> source, Func<T, C> selector) where C : IComparable<C> {
+        var results = new List<T>();
+        using var enumerator = source.GetEnumerator();
+        if (!enumerator.MoveNext()) return results;
+        T maxItem = enumerator.Current;
+        C maxValue = selector(maxItem);
+        results.Add(maxItem);
+        while (enumerator.MoveNext()) {
+            T currentItem = enumerator.Current;
+            C currentValue = selector(currentItem);
+            int comparisonResult = currentValue.CompareTo(maxValue);
+            if (comparisonResult > 0) {
+                maxValue = currentValue;
+                results.Clear();
+                results.Add(currentItem);
+            } else if (comparisonResult == 0) {
+                results.Add(currentItem);
+            }
+        }
+        return results;
+    }
+    /// <summary>
+    /// 选择所有最小值对应的对象。
+    /// 若没有元素则返回空列表。
+    /// </summary>
+    public static List<T> MinByAll<T, C>(this IEnumerable<T> source, Func<T, C> selector) where C : IComparable<C> {
+        var results = new List<T>();
+        using var enumerator = source.GetEnumerator();
+        if (!enumerator.MoveNext()) return results;
+        T minItem = enumerator.Current;
+        C minValue = selector(minItem);
+        results.Add(minItem);
+        while (enumerator.MoveNext()) {
+            T currentItem = enumerator.Current;
+            C currentValue = selector(currentItem);
+            int comparisonResult = currentValue.CompareTo(minValue);
+            if (comparisonResult < 0) {
+                minValue = currentValue;
+                results.Clear();
+                results.Add(currentItem);
+            } else if (comparisonResult == 0) {
+                results.Add(currentItem);
+            }
+        }
+        return results;
+    }
+
+    /// <summary>
+    /// 选择最大值对应的对象。
+    /// 若没有元素则返回 Nothing。
+    /// </summary>
+    public static T MaxBy<T, C>(this IEnumerable<T> source, Func<T, C> selector) where C : IComparable<C> {
+        using var enumerator = source.GetEnumerator();
+        if (!enumerator.MoveNext()) return default;
+        T maxItem = enumerator.Current;
+        C maxValue = selector(maxItem);
+        while (enumerator.MoveNext()) {
+            C value = selector(enumerator.Current);
+            if (value.CompareTo(maxValue) <= 0) continue;
+            maxItem = enumerator.Current;
+            maxValue = value;
+        }
+        return maxItem;
+    }
+    /// <summary>
+    /// 选择最小值对应的对象。
+    /// 若没有元素则返回 Nothing。
+    /// </summary>
+    public static T MinBy<T, C>(this IEnumerable<T> List, Func<T, C> Selector) where C : IComparable<C> {
+        using var enumerator = List.GetEnumerator();
+        if (!enumerator.MoveNext()) { return default; }
+        T minItem = enumerator.Current;
+        C minValue = Selector(minItem);
+        while (enumerator.MoveNext()) {
+            C value = Selector(enumerator.Current);
+            if (value.CompareTo(minValue) >= 0) { continue; }
+            minItem = enumerator.Current;
+            minValue = value;
+        }
+        return minItem;
+    }
+
+    #endregion
+
+    #region Join
+
+    /// <summary>
+    /// 用指定的分割符将集合连接为字符串。
+    /// </summary>
+    public static string Join(this IEnumerable list, char split) {
+        var builder = new StringBuilder();
+        bool isFirst = true;
+        foreach (var element in list) {
+            if (isFirst)
+                isFirst = false;
+            else
+                builder.Append(split);
+            if (element != null) builder.Append(element.ToString());
+        }
+        return builder.ToString();
+    }
+    /// <summary>
+    /// 用指定的分割符将集合连接为字符串。
+    /// </summary>
+    public static string Join(this IEnumerable list, string split) {
+        if (split.IsSingle()) return list.Join(split[0]);
+        var builder = new StringBuilder();
+        bool isFirst = true;
+        foreach (var element in list) {
+            if (isFirst)
+                isFirst = false;
+            else
+                builder.Append(split);
+            if (element != null) builder.Append(element.ToString());
+        }
+        return builder.ToString();
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 判断集合是否有且仅有一个元素。
+    /// </summary>
+    public static bool IsSingle<T>(this IEnumerable<T> source) {
+        if (source == null) return false;
+        if (source is IList<T> list) return list.Count == 1;
+        using var enumerator = source.GetEnumerator();
+        if (!enumerator.MoveNext()) return false;
+        if (enumerator.MoveNext()) return false;
+        return true;
+    }
+
+    /// <summary>
+    /// 对集合的每个元素执行指定操作。
+    /// </summary>
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+        foreach (T item in source) action(item);
+        return source;
+    }
+
 }
---
+++ Utils/DirectoryUtils.cs
@@ -0,0 +1,31 @@
+﻿using MeloongCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeloongCore;
+public static class DirectoryUtils {
+
+    /// <summary>
+    /// 确保路径以 / 或 \ 结尾。
+    /// </summary>
+    public static string ToDirectoryFormat(string folder) => IsDirectoryFormat(folder) ? folder : folder + Path.DirectorySeparatorChar;
+
+    /// <summary>
+    /// 检查路径是否以 / 或 \ 结尾。
+    /// </summary>
+    public static bool IsDirectoryFormat(string path) => path.EndsWithF("/") || path.EndsWithF("\\");
+
+    /// <summary>
+    /// 创建文件夹，或文件所在的文件夹。
+    /// 支持长路径，文件夹已存在时不会抛出异常。
+    /// </summary>
+    public static void Create(string path, bool isFilePath = false) {
+        if (isFilePath) path = Path.GetDirectoryName(path);
+        Directory.CreateDirectory(PathUtils.Shorten(path));
+    }
+
+}
